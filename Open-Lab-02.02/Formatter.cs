243164ffdb752378bd69d26d258bcf559be07de4
pavolhodas <pavolhodas4@gmail.com>
2020-10-14 18:38:30 +0200

    Format double to two decimal numbers

--- Open-Lab-02.02/Formatter.cs
+++ Open-Lab-02.02/Formatter.cs
@@ -6,8 +6,6 @@ namespace Open_Lab_02._02
     {
         public void Format(double[] numbers)
         {
-            //double[] numberss = new double[5] { 10.531, 20.221, 30.4866, 40.4612, 50.987 };
-
                 string space = "";
                 string formattedNumbers = "";
             foreach (double number in numbers)
